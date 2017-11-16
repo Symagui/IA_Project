bb3289b245787d0c55178ba42036553061fffd30
Guillaume <guillaume.ruchot@telecomnancy.net>
2017-11-16 10:34:30 +0100

    Récupération des classes, et de la liste des valeurs

--- script.py
+++ script.py
@@ -1,31 +1,54 @@
+# coding: utf8
+
 import csv
 from itertools import ifilterfalse
 
-classes = [];
+final_classes = [];
 
 ROW_TO_READ = 1;
+
+#Fonction de filtrage des commentaires et des lignes vides dans le fichier census-income.names
 def iscomment(s):
    return s.startswith('|') or not s.strip();
 
+
+#Recuperation des differents attributs, de leurs valeurs possibles et des differentes classes dans le fichier census-income.names
 with open("census-income.names", 'r') as f:
     first = True;
-    i = 0
+    #On recupere les lignes non inutiles
     for line in ifilterfalse(iscomment, f):
+
+        #La première ligne est traitee differement des autres car elle ne contient que les classes finales, et pas de nom supplementaire
         if (first):
             first = not first;
-            classes = [classe.strip() for classe in line.split(', ')]
-            classes[len(classes)-1] = classes[len(classes)-1][:-1]
+            #Un peu de magie qui split la ligne selon le csv, la strip et la mets dans un array
+            final_classes = [classe.strip() for classe in line.split(',')]
+
+            #Suppression du POINT a la fin de la derniere classe
+            final_classes[-1] = final_classes[-1][:-1]
+
+            #Juste un print de test
+            """
             for classe in classes:
                 print(classe);
+            """
         else :
-            break;
-        print("#" + str(i) + ": " + line);
-        i+= 1;
-    print("there were " + str(i) + " line(s).")
 
-print classes;
+            linesplitted = [temp.strip() for temp in line.split(':')]
 
+            #On recupere le nom de la ligne
+            for subline in linesplitted[:-1] :
+                print("name : " + subline)
+            #On recupere la liste des valeurs associees a ce nom
+            else:
+                #Pour chaque valeur on strip et on enlève le POINT de la dernière valeur
+                values = [value.strip() for value in linesplitted[-1].split(',')]
+                values[-1] = values[-1][:-1]
+                print(values)
 
+#print final_classes;
+
+exit()
 #La section suivante traite le document csv data et en sort les valeurs
 with open("census-income.data", 'rb') as csvfile:
 
